23280009447937be038eb238e0e1dc8ab74e0251
Chris Li <211069418+chris-mech@users.noreply.github.com>
2026-03-10 10:47:20 +0000

    feat: add command line args

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -4,15 +4,17 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args[0]);
             Console.WriteLine("Hello, my name is Sam");
             Console.WriteLine("My age is: 32");
             Console.WriteLine("My favourite film is: Blue Velvet");
-            string name = "Chris";
+            string name = args[0];
             Console.WriteLine("My name is: " + name);
-            int age = 28;
+            int age = Int32.Parse(args[1]);
             Console.WriteLine("My age is: " + age);
-            string favouriteFilm = "Marty Supreme";
+            string favouriteFilm = args[2];
             Console.WriteLine("My favourite film is: " + favouriteFilm);
+            
         }
     }
 }
