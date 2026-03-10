7fccd93d34334443b88b69f22ec0e0fa2e5db074
Sam <samuelgdukes@gmail.com>
2026-03-10 11:24:38 +0000

    Add Library Class

---
+++ MyFirstLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstLibrary1
+{
+    public class Class1
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello, from MyFirstLibrary");
+        }
+    }
+}
