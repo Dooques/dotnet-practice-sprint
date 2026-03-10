8479ec80acb19b35583586026fc227905309eae5
Sam <samuelgdukes@gmail.com>
2026-03-10 10:12:50 +0000

    Init commit

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Sam");
+            Console.WriteLine("My age is: 32");
+        }
+    }
+}
