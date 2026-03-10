b94f10ab0122ca618ba1af6b7929af7baf866e67
Chris Li <211069418+chris-mech@users.noreply.github.com>
2026-03-10 10:31:17 +0000

    feat: update name

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, my name is Sam");
             Console.WriteLine("My age is: 32");
             Console.WriteLine("My favourite film is: Blue Velvet");
+            string name = "Chris";
+            Console.WriteLine("My name is: " + name);
         }
     }
 }
